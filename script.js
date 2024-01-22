cd3ac746a051b52f8de5bcd0de6030e050072e4d
pdumpster <112559824+Mateusz-Siwinski@users.noreply.github.com>
2024-01-22 22:43:17 +0100

    root files

---
+++ script.js
@@ -0,0 +1,73 @@
+import index from './wordList.js'
+const para1 = document.querySelector(".pkt");
+const para2 = document.querySelector(".slw");
+const  loadedPoints = localStorage.getItem('points') || 0;
+const  loadedWords = localStorage.getItem('words') || 0;
+let points =+ loadedPoints
+let words =+ loadedWords
+const previousWords = []
+
+
+const input = document.querySelector('#textInput')
+input.addEventListener('keypress', event => {
+  if (event.key === "Enter") {
+    if (input.value.length > 1 && input.value.length < 15) {
+      if (index.includes(input.value.toString().replace(/[^a-zA-Z]/g, '')) === true) {
+        if (!previousWords.includes(input.value.toString())) {
+          previousWords.push(input.value.toString())
+          points+= 300 * input.value.length
+          words++
+          input.style.backgroundColor = "green"
+          input.value = ""
+          writeOut()
+          para1.textContent = `points: ${points}`
+          para2.textContent = `words: ${words}. `
+
+          localStorage.setItem('words', words)
+          localStorage.setItem('points', points)
+          localStorage.setItem('tabledata', JSON.stringify(previousWords));
+
+        }else{
+          input.style.backgroundColor = "red"
+          input.value = ""
+        }
+       }else{
+        input.style.backgroundColor = "red"
+        input.value = ""
+       }
+      }else{
+        input.style.backgroundColor = "red"
+        input.value = ""
+      }
+    }
+  }
+) 
+
+
+
+
+
+function writeOut(){
+  const box = document.querySelector('.box')
+  const para = document.createElement("p");
+
+  previousWords.forEach(word => {
+  para1.textContent = `points: ${points}`
+  para2.textContent = `words: ${words} `
+  para.innerHTML = `${word}<br>`
+  box.appendChild(para)
+});
+}
+
+const storedData = localStorage.getItem('tabledata');
+const loadedTable = JSON.parse(storedData);
+
+for(let word of loadedTable){
+  previousWords.push(word)
+  writeOut()
+}
+
+
+
+
+
